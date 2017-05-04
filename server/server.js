5688d1083aa37f10f5f6cfe7afe07b19ded4c0ea
kelvinnicholls <kelvin.nicholls@gmail.com>
2017-05-04 16:16:47 +0100

    Add socket io

--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
-const path =  require('path');
+const path = require('path');
+const http = require('http'); // required for socket.io, used by express
 const express = require('express');
+const socketIO = require('socket.io');
 
-const publicPath = path.join(__dirname,'../public');
+const publicPath = path.join(__dirname, '../public');
 
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server); // allows us to listen to and emit events between server and client
+
+// gives us acces to http://localhost:3000/socket.io/socket.io.js
+
+io.on('connection', (socket) => {
+    console.log("New user connected");
+    socket.on('disconnect', () => {
+        console.log("Client Disconnected");
+    });
+}); // registers an event listener
+
+
 
 app.use(express.static(publicPath));
 
 let port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Started on port ${port}`);
+server.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
